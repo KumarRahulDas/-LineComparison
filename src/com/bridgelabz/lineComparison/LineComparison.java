888720c1ed85282ff65d42286436d888679d4de8
KumarRahulDas <dasrahulbxrepa7061503554@gmail.com>
2022-12-23 06:27:54 -0800

    Commited Succesfully

---
+++ src/com/bridgelabz/lineComparison/LineComparison.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.lineComparison;
+
+public class LineComparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome To The Line Computation Program");
+    }
+}
